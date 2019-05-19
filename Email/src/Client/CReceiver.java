46f46dedcc6d97176a0374944ee8d1c778456bc0
cyzg <34734034+cyzg@users.noreply.github.com>
2019-05-19 18:33:43 +0800

    最后

--- Email/src/Client/CReceiver.java
+++ Email/src/Client/CReceiver.java
@@ -35,12 +35,12 @@ public class CReceiver extends Thread{
         	//拆包调用
         	DataStruct.APPPackage p = Client.apppackageAnalyse(message);
         	ui.getEmail("Sender："+p.getAuth().getsendID());
-        	ui.getEmail("Time："+p.getAuth().getTimeStamp());
         	String TS = p.getEmail().getcontent();		
 			char M[] = TS.toCharArray();
         	TS = ""+M[0]+M[1]+M[2]+M[3]+"-"+M[4]+M[5]+"-"+M[6]+M[7]+" "+M[8]+M[9]+":"+M[10]+M[11]+":"+M[12]+M[13];
 
-        	ui.getEmail("Email："+TS);
+        	ui.getEmail("Time："+TS);
+        	ui.getEmail("Email："+p.getEmail().getcontent());
         	System.out.println("邮件发信人："+p.getAuth().getsendID());	
         	System.out.println("邮件内容："+p.getEmail().getcontent());	
         }
